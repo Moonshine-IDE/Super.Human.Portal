442c2530cafb9debb6580f71655a61b7d46d24e7
JoelProminic <joel@prominic.net>
2023-08-21 15:41:29 -0500

    Added 'private' property for GenesisDirectory.  #35

--- Super.Human.Portal_Agents/src/main/java/GenesisDirectoryAgents/GenesisDirectoryCreate.java
+++ Super.Human.Portal_Agents/src/main/java/GenesisDirectoryAgents/GenesisDirectoryCreate.java
@@ -1,9 +1,22 @@
 package GenesisDirectoryAgents;
 
+import java.util.Collection;
+
+import com.moonshine.domino.field.FieldDefinition;
+import com.moonshine.domino.util.CustomizationUtils;
+
 /**
  * Modify this class for custom changes to the agent.
  */
 public class GenesisDirectoryCreate extends GenesisDirectoryCreateBase {
 
     // No modifications by default
+	@Override
+	protected Collection<FieldDefinition> getReturnFieldList() {
+		Collection<FieldDefinition> fields = super.getReturnFieldList();
+		CustomizationUtils.removeField(fields, "password");
+
+		return fields;
+		
+	}
 }
