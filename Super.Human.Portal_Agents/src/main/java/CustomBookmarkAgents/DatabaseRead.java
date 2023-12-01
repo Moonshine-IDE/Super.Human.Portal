3cd73c95ed5e855b89d408bca7c9905cf263826b
JoelProminic <joel@prominic.net>
2023-12-01 11:50:32 -0600

    Implemented initial version of hasBookmark logic for #40

--- Super.Human.Portal_Agents/src/main/java/CustomBookmarkAgents/DatabaseRead.java
+++ Super.Human.Portal_Agents/src/main/java/CustomBookmarkAgents/DatabaseRead.java
@@ -1,6 +1,9 @@
 package CustomBookmarkAgents;
 
-import java.util.Random;
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,7 +16,12 @@ import com.moonshine.domino.util.DominoUtils;
 import genesis.LinkProcessor;
 import lotus.domino.Database;
 import lotus.domino.DbDirectory;
+import lotus.domino.Document;
+import lotus.domino.Name;
 import lotus.domino.NotesException;
+import lotus.domino.View;
+import lotus.domino.ViewEntry;
+import lotus.domino.ViewEntryCollection;
 
 /**
  * Return a list of the databases on the server
@@ -21,11 +29,15 @@ import lotus.domino.NotesException;
 public class DatabaseRead extends CRUDAgentBase
 {
 	
+	protected Map<String, Collection<String> > bookmarkCache = null;
+	
 	@Override
 	protected void runAction() {
 		DbDirectory directory = null;
 		JSONArray json = null;
 		try {
+			cacheBookmarks();
+			
 			directory = session.getDbDirectory("");
 			json = new JSONArray();
 			
@@ -86,12 +98,98 @@ public class DatabaseRead extends CRUDAgentBase
 		
 		
 		// properties to track existing values:
-		// TODO:  compute these instead of using placeholders
-		json.put("hasBookmarks", false); // new Random().nextBoolean());
-		json.put("bookmarkCount", 0);
-		json.put("bookmarks", new JSONArray());
+		Collection<String> bookmarks = getBookmarks(db.getServer(), db.getFilePath());
+		boolean hasBookmarks = null != bookmarks && !bookmarks.isEmpty();
+		json.put("hasBookmarks", hasBookmarks); 
+		if (hasBookmarks) {
+			json.put("bookmarkCount", bookmarks.size());
+			JSONArray array = new JSONArray();
+			for (String bookmark : bookmarks) {
+				array.put(bookmark);
+			}
+			json.put("bookmarks", array);
+		}
+		else {
+			json.put("bookmarkCount", 0);
+			json.put("bookmarks", new JSONArray());
+		}
 		
 		return json;
+	}
+	
+	protected void cacheBookmarks() {
+		bookmarkCache = new TreeMap<String, Collection<String> >();
+		View bookmarkView = null;
+		ViewEntryCollection entries = null;
+		try {
+			bookmarkView = DominoUtils.getView(getTargetDatabase(), "All By UNID/CRUD/CustomBookmark");
+			entries = bookmarkView.getAllEntries();
+			ViewEntry curEntry = entries.getFirstEntry();
+			while (null != curEntry) {
+				Document bookmarkDoc = null;
+				try {
+					bookmarkDoc = curEntry.getDocument();
+					String type = bookmarkDoc.getItemValueString("type");
+					if (null != type && type.equalsIgnoreCase("database")) {
+						String server = bookmarkDoc.getItemValueString("server");
+						String dbName = bookmarkDoc.getItemValueString("database");
+						String key = buildDatabaseLookupString(server, dbName);
+						getLog().dbg("Key:  '" + key + "'.");
+						Collection<String> bookmarks = bookmarkCache.get(key);
+						if (null == bookmarks) {
+							bookmarks = new TreeSet<String>();
+							bookmarkCache.put(key, bookmarks);
+						}
+						bookmarks.add(bookmarkDoc.getUniversalID());
+					}
+					// else:  skip other bookmark types
+				}
+				finally {
+					ViewEntry prevEntry = curEntry;
+					curEntry = entries.getNextEntry();
+					
+					DominoUtils.recycle(session, bookmarkDoc);
+					DominoUtils.recycle(session, prevEntry);
+				}
+			}
+		}
+		catch (Exception ex) {
+			getLog().err("Failed to load bookmarks:  ", ex);
+		}
+	}
+	
+	protected String buildDatabaseLookupString(String server, String dbName) {
+		// avoid NullPointerException
+		if (null == server) {
+			server = "";
+		}
+		if (null == dbName) {
+			dbName = "";
+		}
+		
+		return getCommonName(server).toLowerCase() + "!" + dbName.toLowerCase();
+	}
+	
+	protected String getCommonName(String serverName) {
+		if (null == serverName) {
+			return "";
+		}
+		
+		Name name = null;
+		try {
+			name = session.createName(serverName);
+			return name.getCommon();
+		}
+		catch (NotesException ex) {
+			getLog().err("Error in getCommonName:  ", ex);
+			return "";
+		}
+	}
+	
+	protected Collection<String> getBookmarks(String server, String dbName) {
+		String key = buildDatabaseLookupString(server, dbName);
+		getLog().dbg("Key:  '" + key + "'.");
+		return bookmarkCache.get(key);
 	}
 	
 	@Override
